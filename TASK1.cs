4dbad1f1d3709e06a0cc70a7bc4c57d9d5b80d10
Варочкина Галя <147079170+varochkinagalia@users.noreply.github.com>
2023-10-24 19:16:23 +0300

    Add files via upload

---
+++ TASK1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int K = 2;
+            int[] massiv = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            int[] vstavka = new int[] { 67, 78, 99 };
+            int N = massiv.Length;
+            int M = vstavka.Length;
+            Array.Resize(ref massiv, massiv.Length + M);
+            Array.Copy(massiv, K, massiv, K+M, N-K);
+            Array.Copy(vstavka, 0, massiv,K, M);
+            foreach (int i in massiv)
+            {
+                Console.Write(i+" ");
+            }
+
+
+        }
+    }
+}
